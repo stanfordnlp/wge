4d14b82ce2f7a22faaad0c1cb36716dd232fb9cb
ezliu <evanliu@stanford.edu>
2020-05-15 22:15:15 +0000

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(name="python3_gtd",
+      version="1.0",
+      description="Python3 version of Kelvin Guu's gtd library",
+      packages=["gtd"],
+      license="LICENSE")
